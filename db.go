fceb364e01089a21182ef205d9679ad83b988a6c
HoLLy <dario.db@hotmail.com>
2020-02-11 16:01:06 +0100

    update urls

--- db.go
+++ db.go
@@ -11,7 +11,7 @@ import (
 	"time"
 )
 
-const databaseUrl = "my-release-dgraph-alpha.acubed:9080"
+const databaseUrl = "dgraph-public:9080"
 
 func newClient() *dgo.Dgraph {
 	// Dial a gRPC connection. The address to dial to can be configured when
