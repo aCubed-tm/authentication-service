5d602a5a53531f42499b5d0d7a8660139d585bd9
HoLLy <dario.db@hotmail.com>
2020-02-13 12:21:02 +0100

    Add helper functions for database access to make code more C L E A N

--- db.go
+++ db.go
@@ -12,10 +12,6 @@ const password = ""
 
 var driver neo4j.Driver = nil
 
-func newSessionRead() neo4j.Session  { return newSession(neo4j.AccessModeRead) }
-func newSessionWrite() neo4j.Session { return newSession(neo4j.AccessModeWrite) }
-
-// e.g. neo4j.AccessModeWrite
 func newSession(accessMode neo4j.AccessMode) neo4j.Session {
 	var (
 		err     error
@@ -38,158 +34,64 @@ func newSession(accessMode neo4j.AccessMode) neo4j.Session {
 }
 
 func CheckEmailExists(email string) (bool, error) {
-	q := "MATCH (e:Email {emailAddress: $email}) RETURN COUNT(e)"
+	query := "MATCH (e:Email {emailAddress: $email}) RETURN COUNT(e)"
 	variables := map[string]interface{}{"email": email}
-
-	count, err := newSessionRead().ReadTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
-		result, err := transaction.Run(q, variables)
-		if err != nil {
-			return nil, err
-		}
-
-		if result.Next() {
-			return result.Record().GetByIndex(0), nil
-		}
-
-		return nil, result.Err()
-	})
-	if err != nil {
-		return false, err
-	}
-
-	return count.(int) > 0, nil
+	count, err := FetchSingle(query, variables)
+	return count.(int) > 0, err
 }
 
 func VerifyEmailByToken(token string, verificationTime time.Time) error {
-	q := "MATCH (e:Email {verificationToken: $token}) SET e.verifiedAt = $date"
+	query := "MATCH (e:Email {verificationToken: $token}) SET e.verifiedAt = $date"
 	variables := map[string]interface{}{"token": token, "date": verificationTime}
-
-	_, err := newSessionWrite().WriteTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
-		_, err := transaction.Run(q, variables)
-		return nil, err
-	})
-	return err
+	return Write(query, variables)
 }
 
 func GetAllEmailsByUuid(uuid string) ([]string, error) {
-	q := "MATCH (:Account {uuid: $uuid})--(e:Email) RETURN e.emailAddress"
+	query := "MATCH (:Account {uuid: $uuid})--(e:Email) RETURN e.emailAddress"
 	variables := map[string]interface{}{"uuid": uuid}
-
-	result, err := newSessionRead().ReadTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
-		result, err := transaction.Run(q, variables)
-		if err != nil {
-			return nil, err
-		}
-
-		var ret []string
-		for result.Next() {
-			rec := result.Record()
-			ret = append(ret, rec.GetByIndex(0).(string))
-		}
-
-		return ret, nil
-	})
-	return result.([]string), err
+	return FetchStringArray(query, variables)
 }
 
 func GetPasswordByEmail(email string) (string, error) {
-	q := "MATCH (:Email {emailAddress: $email})--(a:Account) RETURN a.password"
+	query := "MATCH (:Email {emailAddress: $email})--(a:Account) RETURN a.password"
 	variables := map[string]interface{}{"email": email}
-
-	password, err := newSessionRead().ReadTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
-		result, err := transaction.Run(q, variables)
-		if err != nil {
-			return nil, err
-		}
-
-		if result.Next() {
-			return result.Record().GetByIndex(0), nil
-		}
-
-		return nil, result.Err()
-	})
+	password, err := FetchSingle(query, variables)
 	return password.(string), err
 }
 
 func GetInviteOrganizationsByEmail(email string) ([]string, error) {
-	q := "MATCH (:Email{emailAddress: $email})<-[:INVITED]-(o:Organisation) RETURN o.uuid"
+	query := "MATCH (:Email{emailAddress: $email})<-[:INVITED]-(o:Organisation) RETURN o.uuid"
 	variables := map[string]interface{}{"email": email}
-	invites, err := newSessionRead().ReadTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
-		result, err := transaction.Run(q, variables)
-		if err != nil {
-			return nil, err
-		}
-
-		var ret []string
-		for result.Next() {
-			rec := result.Record()
-			ret = append(ret, rec.GetByIndex(0).(string))
-		}
-
-		return ret, nil
-	})
-	return invites.([]string), err
+	return FetchStringArray(query, variables)
 }
 
 func GetUuidByEmail(email string) (string, error) {
-	q := "MATCH (:Email{emailAddress: $email})<-[:HAS_EMAIL]-(a:Account) RETURN a.uuid"
+	query := "MATCH (:Email{emailAddress: $email})<-[:HAS_EMAIL]-(a:Account) RETURN a.uuid"
 	variables := map[string]interface{}{"email": email}
-
-	uuid, err := newSessionRead().ReadTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
-		result, err := transaction.Run(q, variables)
-		if err != nil {
-			return nil, err
-		}
-
-		if result.Next() {
-			return result.Record().GetByIndex(0), nil
-		}
-
-		return nil, result.Err()
-	})
+	uuid, err := FetchSingle(query, variables)
 	return uuid.(string), err
 }
 
 func ChangePasswordForEmail(email string, password string) error {
-	q := "MATCH (:Email {emailAddress: $email})<-[:HAS_EMAIL]-(a:Account) SET a.password = $password"
+	query := "MATCH (:Email {emailAddress: $email})<-[:HAS_EMAIL]-(a:Account) SET a.password = $password"
 	variables := map[string]interface{}{"email": email, "password": password}
-
-	_, err := newSessionWrite().WriteTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
-		_, err := transaction.Run(q, variables)
-		return nil, err
-	})
-	return err
+	return Write(query, variables)
 }
 
 func DropJwtToken(token string) error {
-	q := "MATCH (t:JWTToken{token: $token}) DETACH DELETE t"
+	query := "MATCH (t:JWTToken{token: $token}) DETACH DELETE t"
 	variables := map[string]interface{}{"token": token}
-
-	_, err := newSessionWrite().WriteTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
-		_, err := transaction.Run(q, variables)
-		return nil, err
-	})
-	return err
+	return Write(query, variables)
 }
 
 func DropAllTokensForUuid(uuid string) error {
-	q := "MATCH (:Account {uuid: $uuid})-[:HAS_TOKEN]->(t:JWTToken) DETACH DELETE t"
+	query := "MATCH (:Account {uuid: $uuid})-[:HAS_TOKEN]->(t:JWTToken) DETACH DELETE t"
 	variables := map[string]interface{}{"$uuid": uuid}
-
-	_, err := newSessionWrite().WriteTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
-		_, err := transaction.Run(q, variables)
-		return nil, err
-	})
-	return err
+	return Write(query, variables)
 }
 
 func AddJwtTokenToUser(uuid string, token string) error {
-	q := "MATCH (a:Account {uuid: $uuid}) CREATE (a)-[:HAS_TOKEN]->(t:JWTToken{token: $token})"
+	query := "MATCH (a:Account {uuid: $uuid}) CREATE (a)-[:HAS_TOKEN]->(t:JWTToken{token: $token})"
 	variables := map[string]interface{}{"$token": token, "$uuid": uuid}
-
-	_, err := newSessionWrite().WriteTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
-		_, err := transaction.Run(q, variables)
-		return nil, err
-	})
-	return err
+	return Write(query, variables)
 }
